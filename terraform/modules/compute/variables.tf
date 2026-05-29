50feef5ebcd6beebc23a942ce4cdf91bd134cd71
Oluwakemi Akindele <brandykemi@gmail.com>
2026-05-29 23:07:44 +0200

    Update variables.tf

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,10 +1,32 @@
-variable "project_name" { type = string }
-variable "vpc_id" { type = string }
-variable "public_subnet_ids" { type = list(string) }
-variable "private_subnet_ids" { type = list(string) }
-variable "instance_type" { type = string }
-variable "key_pair_name" { type = string }
-variable "my_ip_address" { type = string }
-variable "mongo_uri" { type = string; sensitive = true }
-variable "jwt_secret" { type = string; sensitive = true }
-variable "aws_region" { type = string }
+variable "project_name" {
+  type = string
+}
+variable "vpc_id" {
+  type = string
+}
+variable "public_subnet_ids" {
+  type = list(string)
+}
+variable "private_subnet_ids" {
+  type = list(string)
+}
+variable "instance_type" {
+  type = string
+}
+variable "key_pair_name" {
+  type = string
+}
+variable "my_ip_address" {
+  type = string
+}
+variable "mongo_uri" {
+  type      = string
+  sensitive = true
+}
+variable "jwt_secret" {
+  type      = string
+  sensitive = true
+}
+variable "aws_region" {
+  type = string
+}
